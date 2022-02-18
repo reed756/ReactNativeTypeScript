37b738cce4f2b6b8c8a6408334e4f977f860b4d5
manntom37 <thomasemann4@gmail.com>
2022-02-18 14:42:17 +0000

    auth functioning

--- App.tsx
+++ App.tsx
@@ -1,87 +1,31 @@
-import * as React from "react";
-import { FC, useEffect, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  FlatList,
-  Button,
-} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import Amplify from "aws-amplify";
-import awsconfig from "./src/aws-exports";
+import config from "./src/aws-exports";
+// @ts-ignore
 import { withAuthenticator } from "aws-amplify-react-native";
-import { Authenticator } from "@aws-amplify/ui-react";
-Amplify.configure(awsconfig);
-
-// import Input from "./Input";
-// import { Fruit, Fruits } from "./data";
-// import Item from "./ListItem";
-
-// AKIA3KDFAUFYQ4FLPLKV
-// 4U5HxNLi5dE16vyq6/bDhseD2sVaBlyp/P7pn0aH
-
-const App: FC = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text>Hello World</Text>
-        <Authenticator>
-          {({ signOut, user }) => (
-            <View>
-              <Text>
-                Hey {user.username}, welcome to my channel, with auth!
-              </Text>
-              <Button onPress={signOut} title="Sign out" />
-            </View>
-          )}
-        </Authenticator>
-      </View>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    paddingVertical: 10,
+import Home from "./Home";
+Amplify.configure({
+  ...config,
+  Analytics: {
+    disabled: true,
   },
 });
-
-export default withAuthenticator(App);
-
-{
-  /* <View style={styles.container}>
-        <Input
-          icon="md-search"
-          placeholder="Search"
-          onChangeText={(text) => {
-            console.log(text);
-          }}
-        />
-        <FlatList
-          style={{ marginVertical: 10 }}
-          data={Fruits}
-          renderItem={({ item }) => (
-            <Item id={item.id} name={item.name} price={item.price} />
-          )}
-        />
-      </View> */
+function App() {
+  return (
+    <View style={styles.container}>
+     
+      <StatusBar style="auto" />
+       <Home />
+    </View>
+  );
 }
-
-// const [searchQuery, setSearchQuery] = useState();
-// const [Fruits, setFruits] = useState<Fruit[] | null>(null);
-
-// useEffect(() => {
-//   (() => {
-//     setFruits(Fruits);
-//   })();
-// }, []);
-
-// const handleSearch = (text) => {
-//   const fruits: Fruits[] = Fruits.filter((fruit) =>
-//     fruit.name.includes(text)
-//   );
-//   setFruits(fruits);
-// };
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+export default withAuthenticator(App);
