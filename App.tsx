af185992c0587f641910c47eac42f7ec0fa69b8c
asimditta <asimditta15@gmail.com>
2022-02-21 10:46:43 +0000

    add reducer store

--- App.tsx
+++ App.tsx
@@ -3,22 +3,29 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import Amplify from "aws-amplify";
 import config from "./src/aws-exports";
+import { Auth } from "aws-amplify";
 // @ts-ignore
 import { withAuthenticator } from "aws-amplify-react-native";
-import Home from "./Home";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import Home from "./components/Home";
+import { allReducers } from "./reducers";
+const store = createStore(allReducers);
 Amplify.configure({
   ...config,
   Analytics: {
     disabled: true,
   },
 });
+
 function App() {
   return (
-    <View style={styles.container}>
-     
-      <StatusBar style="auto" />
-       <Home />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Home />
+      </View>
+    </Provider>
   );
 }
 const styles = StyleSheet.create({
